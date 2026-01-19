7214411ffa2b6b56a766df7e9af98f10205b0330
urmatru <mylacked@gmail.com>
2026-01-19 15:50:20 +0500

    Ready to upgrade, modify

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from reviews.models import Review
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    recent_reviews = Review.objects.select_related('movie', 'user').order_by('-created_at')[:10]
+
+    context = {
+        "recent_reviews" : recent_reviews
+    }
+    return render(request, "home.html", context)
